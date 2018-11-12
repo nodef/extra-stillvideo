b490706cb85fea00d6772d22ce2f29648c738e43
Subhajit Sahu <wolfram77@gmail.com>
2018-11-12 12:40:17 +0530

    stillvideo(): does not accept URL anymore

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-const randomItem = require('random-item');
-const download = require('download');
 const boolean = require('boolean');
-const tempy = require('tempy');
-const urlParse = require('url').parse;
+const _ = require('lodash');
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
@@ -11,40 +8,24 @@ const fs = require('fs');
 
 // Global variables
 const E = process.env;
-const LOG = boolean(E['STILLVIDEO_LOG']||'0');
-const VIDEO = {
-  loop: parseFloat(E['STILLVIDEO_VIDEO_LOOP']||'1'),
-  framerate: parseFloat(E['STILLVIDEO_VIDEO_FRAMERATE']||'1'),
-  vcodec: E['STILLVIDEO_VIDEO_VCODEC']||'libx264',
-  crf: E['STILLVIDEO_VIDEO_CRF']||'0',
-  preset: E['STILLVIDEO_VIDEO_PRESET']||'veryfast',
-  tune: E['STILLVIDEO_VIDEO_TUNE']||'stillimage',
-  acodec: E['STILLVIDEO_VIDEO_ACODEC']||'copy'
+const OPTIONS = {
+  log: boolean(E['STILLVIDEO_LOG']||'0'),
+  video: {
+    loop: parseFloat(E['STILLVIDEO_VIDEO_LOOP']||'1'),
+    framerate: parseFloat(E['STILLVIDEO_VIDEO_FRAMERATE']||'1'),
+    vcodec: E['STILLVIDEO_VIDEO_VCODEC']||'libx264',
+    crf: E['STILLVIDEO_VIDEO_CRF']||'0',
+    preset: E['STILLVIDEO_VIDEO_PRESET']||'veryfast',
+    tune: E['STILLVIDEO_VIDEO_TUNE']||'stillimage',
+    acodec: E['STILLVIDEO_VIDEO_ACODEC']||'copy',
+    cp: null
+  }
 };
 const CP = {
   sync: true,
   stdio: [0, 1, 2]
 };
-const IMAGE_CONVERT = {
-  apiKey: E['FILESTACK_APIKEY']||''
-};
-const FN_NOP = () => 0;
 
-
-// Get filename, without extension.
-function pathFilename(pth) {
-  return pth.substring(0, pth.length-path.extname(pth).length);
-};
-
-// Copy file, return promise.
-function fsCopyFile(src, dst, flags=0) {
-  return new Promise((fres, frej) => {
-    fs.copyFile(src, dst, flags, (err) => {
-      if(err) frej(err);
-      else fres(dst);
-    });
-  });
-};
 
 // Execute child process, return promise.
 function cpExec(cmd, o) {
@@ -57,42 +38,6 @@ function cpExec(cmd, o) {
   });
 };
 
-// Download file, return promise.
-function fileDownload(out, url) {
-  if(LOG) console.log('fileDownload:', out, url);
-  return download(url, path.dirname(out), {filename: path.basename(out)}).then(() => out);
-};
-
-// Convert image URL to given format.
-function imageConvert(url, fmt, o) {
-  o = Object.assign({}, IMAGE_CONVERT, o);
-  if(LOG) console.log('imageConvert:', url, fmt);
-  var key = randomItem((o.apiKey||'').split(';'));
-  var inp = path.extname(urlParse(url).pathname).substring(1).toLowerCase()||'png';
-  return [fmt, 'jpg', 'png'].includes(inp)? url:`https://process.filestackapi.com/${key}/output=format:${fmt}/${url}`;
-};
-
-// Generate output image file.
-function outputImage(out, src, o) {
-  var o = o||{};
-  if(LOG) console.log('outputImage:', out);
-  var dst = pathFilename(out)+path.extname(src);
-  if(!src.includes('://')) {
-    if(dst===src) return Promise.resolve(dst);
-    else return fsCopyFile(src, dst);
-  }
-  var fmt = path.extname(out).substring(1);
-  var url = imageConvert(src, fmt, o.convert);
-  return fileDownload(out, url);
-};
-
-// Generate output video file.
-function outputVideo(out, aud, img, o) {
-  var o = Object.assign({}, VIDEO, o);
-  if(LOG) console.log('outputVideo:', out);
-  return cpExec(`ffmpeg -y -err_detect explode -loop ${o.loop} -framerate ${o.framerate} -i "${img}" -i "${aud}" -vcodec ${o.vcodec} -crf ${o.crf} -preset ${o.preset} -tune ${o.tune} -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -acodec ${o.acodec} -shortest "${out}"`, o.cp||{}).then(() => out);
-};
-
 /**
  * Generate still video from an audio and image file, through machine (via "ffmpeg").
  * @param {string} out output video file.
@@ -101,15 +46,12 @@ function outputVideo(out, aud, img, o) {
  * @param {object} o options.
  * @returns promise <out> when done.
  */
-async function stillvideo(out, aud, img, o) {
-  var o = o||{}, imgo = img;
-  if(LOG) console.log('@video:', out, aud, img, o);
-  var imge = img? path.extname(img):'.jpg';
-  var imgp = tempy.file({extension: imge.substring(1)});
-  if(img) img = await outputImage(imgp, img, o.image);
-  if(img) await outputVideo(out, aud, img, o.video);
-  if(imgo && imgo!==img) fs.unlink(imgo, FN_NOP);
-  return out;
+function stillvideo(out, aud, img, o) {
+  var o = _.merge({}, OPTIONS, o);
+  var l = o.log, v = o.video;
+  var cmd = `ffmpeg -y -loop ${v.loop} -framerate ${v.framerate} -i "${img}" -i "${aud}" -vcodec ${v.vcodec} -crf ${v.crf} -preset ${v.preset} -tune ${v.tune} -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -acodec ${v.acodec} -shortest "${out}"`;
+  if(l) { console.log('@video:', out, aud, img); console.log('-cpExec:', cmd); }
+  return cpExec(cmd, l? Object.assign({}, a.cp, CP):a.cp).then(() => out);
 };
 module.exports = stillvideo;
 
