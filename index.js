28d6487f9895b4af9c791ebc2633c73bb16191c5
Subhajit Sahu <wolfram77@gmail.com>
2018-11-16 20:52:40 +0530

    imageTransform: the image bouding box

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const boolean = require('boolean');
 const tempy = require('tempy');
+const Jimp = require('jimp')
 const _ = require('lodash');
 const cp = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 
 // Global variables
@@ -11,7 +14,11 @@ const STDIO = [0, 1, 2];
 const OPTIONS = {
   stdio: null,
   help: false,
+  input: null,
   log: boolean(E['STILLVIDEO_LOG']||'0'),
+  output: E['STILLVIDEO_OUTPUT']||'out.mp4',
+  audio: E['STILLVIDEO_AUDIO'],
+  image: E['STILLVIDEO_IMAGE'],
   loop: parseFloat(E['STILLVIDEO_LOOP']||'1'),
   framerate: parseFloat(E['STILLVIDEO_FRAMERATE']||'1'),
   vcodec: E['STILLVIDEO_VCODEC']||'libx264',
@@ -24,6 +31,7 @@ const OPTIONS = {
   fitX: E['STILLVIDEO_FITX'],
   fitY: E['STILLVIDEO_FITY']
 };
+const FN_NOP = () => 0;
 
 
 // Execute child process, return promise.
@@ -36,6 +44,20 @@ function cpExec(cmd, o) {
   }));
 };
 
+// Transform image to proper size.
+async function imageTransform(pth, o) {
+  var ext = path.extname(pth);
+  var img = await Jimp.read(pth);
+  var w0 = img.bitmap.width, h0 = img.bitmap.height;
+  if(o.resizeX || o.resizeY) img.resize(o.resizeX||Jimp.AUTO, o.resizeY||Jimp.AUTO);
+  if(o.fitX && o.fitY) img.scaleToFit(o.fitX, o.fitY);
+  pth = tempy.file({extension: 'jpg'});
+  await img.write(pth);
+  var w1 = img.bitmap.width, h1 = img.bitmap.height;
+  if(o.log) console.log('-imageTransform:', `${ext} ${w0}x${h0} -> jpg ${w1}x${h1}`);
+  return pth;
+};
+
 /**
  * Generate still video from an audio and image file, through machine (via "ffmpeg").
  * @param {string} out output video file.
@@ -46,29 +68,45 @@ function cpExec(cmd, o) {
  */
 async function stillvideo(out, aud, img, o) {
   var o = _.merge({}, OPTIONS, o);
+  var out = out||o.output, aud = aud||o.audio, img = img||o.image;
   if(o.log) console.log('@stillvideo:', out, aud, img);
-  return cpExec(`ffmpeg -y -loop ${o.loop} -framerate ${o.framerate} -i "${img}" -i "${aud}" -vcodec ${o.vcodec} -crf ${o.crf} -preset ${o.preset} -tune ${o.tune} -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -acodec ${o.acodec} -shortest "${out}"`, o);
+  var imgt = await imageTransform(img, o);
+  var z = await cpExec(`ffmpeg -y -loop ${o.loop} -framerate ${o.framerate} -i "${imgt}" -i "${aud}" -vcodec ${o.vcodec} -crf ${o.crf} -preset ${o.preset} -tune ${o.tune} -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -acodec ${o.acodec} -shortest "${out}"`, o);
+  if(imgt!==img) fs.unlink(imgt, FN_NOP);
+  return z;
 };
+
+// Get options from arguments.
+function options(a, z={}) {
+  for(var i=2, I=a.length; i<I; i++) {
+    if(a[i]==='--help') z.help = true;
+    else if(a[i]==='-l' || a[i]==='--log') z.log = true;
+    else if(a[i]==='-o' || a[i]==='--output') z.output = a[++i];
+    else if(a[i]==='-a' || a[i]==='--audio') z.audio = a[++i];
+    else if(a[i]==='-i' || a[i]==='--image') z.image = a[++i];
+    else if(a[i]==='-ol' || a[i]==='--loop') z.loop = parseInt(a[++i], 10);
+    else if(a[i]==='-of' || a[i]==='--framerate') z.framerate = parseFloat(a[++i]);
+    else if(a[i]==='-ov' || a[i]==='--vcodec') z.vcodec = a[++i];
+    else if(a[i]==='-oc' || a[i]==='--crf') z.crf = a[++i];
+    else if(a[i]==='-op' || a[i]==='--preset') z.preset = a[++i];
+    else if(a[i]==='-ot' || a[i]==='--tune') z.tune = a[++i];
+    else if(a[i]==='-oa' || a[i]==='--acodec') z.acodec = a[++i];
+    else if(a[i]==='-rx' || a[i]==='--resizex') z.resizeX = parseFloat(a[++i]);
+    else if(a[i]==='-ry' || a[i]==='--resizey') z.resizeY = parseFloat(a[++i]);
+    else if(a[i]==='-fx' || a[i]==='--fitx') z.fitX = parseFloat(a[++i]);
+    else if(a[i]==='-fy' || a[i]==='--fity') z.fitY = parseFloat(a[++i]);
+    else z.input = a[i];
+  }
+  return z;
+};
+stillvideo.options = options;
 module.exports = stillvideo;
 
-// Run on console.
-function shell(A) {
-  var out = 'out.mp4', aud = '', img = '', o = {};
-  for(var i=2, I=A.length; i<I; i++) {
-    if(A[i]==='--help') return cp.execSync('less README.md', {cwd: __dirname, stdio: [0, 1, 2]});
-    else if(A[i]==='-o' || A[i]==='--output') out = A[++i];
-    else if(A[i]==='-a' || A[i]==='--audio') aud = A[++i];
-    else if(A[i]==='-i' || A[i]==='--image') img = A[++i];
-    else if(A[i]==='-l' || A[i]==='--log') _.merge(o, {log: true});
-    else if(A[i]==='-ol' || A[i]==='--loop') _.merge(o, {loop: parseInt(A[++i], 10)});
-    else if(A[i]==='-of' || A[i]==='--framerate') _.merge(o, {framerate: parseFloat(A[++i])});
-    else if(A[i]==='-ov' || A[i]==='--vcodec') _.merge(o, {vcodec: A[++i]});
-    else if(A[i]==='-oc' || A[i]==='--crf') _.merge(o, {crf: A[++i]});
-    else if(A[i]==='-op' || A[i]==='--preset') _.merge(o, {preset: A[++i]});
-    else if(A[i]==='-ot' || A[i]==='--tune') _.merge(o, {tune: A[++i]});
-    else if(A[i]==='-oa' || A[i]==='--acodec') _.merge(o, {acodec: A[++i]});
-    else return console.error(`stillvideo: Unexpected input "${A[i]}"`);
-  }
-  return stillvideo(out, aud, img, o);
+// Run on shell.
+function shell(a) {
+  var o = options(a);
+  if(o.help) return cp.execSync('less README.md', {cwd: __dirname, stdio: STDIO});
+  else if(o.input) return console.error(`@stillvideo: Unexpected input "${A[i]}"`);
+  return stillvideo(null, null, null, o);
 };
 if(require.main===module) shell(process.argv);
